c0c0462200ec72340f652ec1e0c291c40ff97d2a
Christian Renoulin <chris-ren@netcourrier.com>
2016-12-29 23:03:29 +0100

    Carte un peu mieux

--- www/js/controller/CarteCtrl.js
+++ www/js/controller/CarteCtrl.js
@@ -1,191 +1,111 @@
 /* CARTE */
 
 angular.module('starter.controllers')
-.controller('CarteCtrl', 
-            function($scope, $state, RechercheService, ParamService) {
-                
-       //ServiceStructures, leafletData, leafletMapEvents
-       // $scope.modeCollecte = [ { "code" : "smco_conteneurlerelais", "libelle" : "Conteneur Le Relais" }, { "code" : "modco_contpapiercarton", "libelle" : "Conteneur papier/carton"} ];
-      $scope.modeCollecte = RechercheService.getAFilter("filter_map");
+.controller('CarteCtrl',
+	function ($scope, $state, RechercheService, ParamService) {
 
-      $scope.onSearchSubmit = function() {
-        var temp = "";
+	$scope.tousModesDeCollecte = RechercheService.getAFilter("filter_map");
 
-        angular.forEach($scope.modeCollecte, function(val, index) {
-            if (!angular.isUndefined($scope.modeCollecte[index].checked) && $scope.modeCollecte[index].checked) {
-              temp += $scope.modeCollecte[index].id+",";
-            }
-        })
-        if (temp.length>0) {
-            temp = temp.substring(0,temp.length-1);
-        }
-        ParamService.setValueInLocalStorage("filter_map_checked", temp);
-        $state.go('tab.carte-detail');
-     };
+	$scope.onSearchSubmit = function () {
+		var temp = "";
 
-            //CONTENEURS
-            
-            //$scope.markers = ServiceStructures.getLeafletContainers($scope.center); 
+		angular.forEach($scope.tousModesDeCollecte, function (val, index) {
+			if (!angular.isUndefined($scope.tousModesDeCollecte[index].checked) && $scope.tousModesDeCollecte[index].checked) {
+				temp += $scope.tousModesDeCollecte[index].id + ",";
+			}
+		})
+		if (temp.length > 0) {
+			temp = temp.substring(0, temp.length - 1);
+		}
+		ParamService.setValueInLocalStorage("filter_map_checked", temp);
+		$state.go('tab.carte-detail');
+	};
 
-            //var leafletEventName = 'leafletDirectiveMap.moveend';
-            /*
-            $scope.$on(leafletEventName, function(event) {
-                    //$scope.eventDetected = event.name;                     
-                    leafletData.getMap().then(function(map) {
-
-                           $scope.markers = ServiceStructures.getLeafletContainers(map.getCenter()); 
-                           var circle= {
-                                type: "circle",
-                                radius: 1200,
-                                latlngs: map.getCenter(),
-                                color : 'green',
-                                fillColor : '#c2e47e',
-                                fillOpacity : 0.3,
-                                weight : 3
-                           };
-                           $scope.paths = {};
-                           $scope.paths['circle'] = circle;
-
-                    });
-
-            });*/
-    
 })
-.controller('CarteDetailCtrl', 
-            function($scope, $stateParams, leafletData, leafletMarkerEvents, CarteService, ParamService, RechercheService) {
-            
-      var filterMap = ParamService.getValueInLocalStorage("filter_map_checked");
-      var modeCollecte = RechercheService.getAFilter("filter_map");
-      var listFilterMap = filterMap.split(",");
-      var codesModeCollecte = "";
-      var labelsModeCollecte = "";
-      var markersCircle = new Array();
+.controller('CarteDetailCtrl',
+	function ($scope, $stateParams, leafletData, leafletMarkerEvents, CarteService, ParamService, RechercheService) {
 
-      for (var i=0; i<modeCollecte.length;i++) {
-            var ajoutModeCollecte = false;
-            for (var j=0; j<listFilterMap.length && ajoutModeCollecte===false;j++) {
-              if (modeCollecte[i].id===listFilterMap[j]) {
-                ajoutModeCollecte = true;
-              }
-            }
-            if (ajoutModeCollecte) {
-              codesModeCollecte  += modeCollecte[i].code + ",";
-              labelsModeCollecte += modeCollecte[i].nom + ", ";
-            }
-        }
+	var filterMap = ParamService.getValueInLocalStorage("filter_map_checked");
+	var tousModesDeCollecte = RechercheService.getAFilter("filter_map");
+	var listFilterMap = filterMap.split(",");
+	var idsModeCollecte = "";
+	var labelsModeCollecte = "";
 
-      labelsModeCollecte = labelsModeCollecte.substring(0, labelsModeCollecte.length -2);
+	for (var i = 0; i < tousModesDeCollecte.length; i++) {
+		var ajoutModeCollecte = false;
+		for (var j = 0; j < listFilterMap.length && ajoutModeCollecte === false; j++) {
+			if (tousModesDeCollecte[i].id === listFilterMap[j]) {
+				ajoutModeCollecte = true;
+			}
+		}
+		if (ajoutModeCollecte) {
+			idsModeCollecte += tousModesDeCollecte[i].id + ",";
+			labelsModeCollecte += tousModesDeCollecte[i].nom + ", ";
+		}
+	}
 
+	labelsModeCollecte = labelsModeCollecte.substring(0, labelsModeCollecte.length - 2);
 
-      // var temp=0;
-     
-            /* INIT CARTE */
-            $scope.center = {
-                    lat: ParamService.getNumberParam("geo.defaultLat", 47.240987),
-                    lng: ParamService.getNumberParam("geo.defaultLong", -1.8),
-                    zoom: ParamService.getNumberParam("geo.zoomInit", 10)
-            };
-
-            $scope.centerInit = angular.copy($scope.center);
-
-            angular.extend($scope, {
-                center: $scope.center,
-                markers : CarteService.getLeafletContainers($scope.center, codesModeCollecte),
-                layers: {
-                        baselayers: {
-                            openStreetMap: {
-                                name: 'OpenStreetMap',
-                                type: 'xyz',
-                                url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                            }
-                        },
-                },            
-                events: {
-                    map: {
-                        enable: ['moveend'],
-                        logic: 'emit'
-                    }
-                }
-                
-            });
-            
-            //Récupération des marqueurs Leaflets
-            $scope.labelsModeCollecte = labelsModeCollecte;
-
-            /* Réaction évenement movend sur la carte */
-            $scope.$on('leafletDirectiveMap.mymap.moveend', function(event){
-
-                leafletData.getMap("mymap").then(
-                    function (map) {
-                       
-                        var distanceAvantRedessiner = ParamService.getNumberParam('geo.zoneDistanceAvantRedessiner', 1000);
-                        if (pointEstDansZoneLimitee(map.getCenter().lat, map.getCenter().lng, distanceAvantRedessiner) == false) {
-
-                            $scope.markers = CarteService.getLeafletContainers($scope.center, codesModeCollecte);
-                            $scope.centerInit = angular.copy($scope.center);
-                            
-                        }
-                        ajouteCercleZoneLimitee(map.getCenter().lat, map.getCenter().lng);                      
-
-                    }
-                );
-
-            });
+	var local_icons = {
+		default_icon: {},
+		reemploi: {
+			iconUrl: "resources/icons/marker-icon-blue.png"
+		},
+		conteneurs: {
+			iconUrl: "resources/icons/marker-icon-brown.png"
+		},
+		decheterie: {
+			iconUrl: "resources/icons/marker-icon-green.png"
+		},
+		composteurs: {
+			iconUrl: "resources/icons/marker-icon-yellow.png"
+		},
+		conteneurlerelais: {
+			iconUrl: "resources/icons/marker-icon-pink.png"
+		},
+		ventevrac: {
+			iconUrl: "resources/icons/marker-icon-red.png"
+		},
+		trisac: {
+			iconUrl: "resources/icons/marker-icon-purple.png"
+		}
+	};
+	angular.extend($scope, {
+		icons: local_icons
+	});
 
 
-            /**
-             * Vérifie si les coordonnées en paramètre correspondent à un point de la
-             * zône limitée.<br/> Invoque estDansZoneLimitee .
-             */
-            var pointEstDansZoneLimitee = function(lat, lng, distance) {
+	/* INIT CARTE */
+	$scope.center = {
+		lat: ParamService.getNumberParam("geo.defaultLat", 47.240987),
+		lng: ParamService.getNumberParam("geo.defaultLong", -1.8),
+		zoom: ParamService.getNumberParam("geo.zoomInit", 10)
+	};
 
-                var latlngcurrent = L.latLng(lat, lng);               
-                var latlngInit = L.latLng($scope.centerInit.lat, $scope.centerInit.lng);
+	$scope.centerInit = angular.copy($scope.center);
 
-                var distanceEnMetre = latlngcurrent.distanceTo(latlngInit);
-                var resultat = distanceEnMetre < distance;
+	angular.extend($scope, {
+		center: $scope.center,
+		markers: CarteService.getLeafletContainers($scope.center, idsModeCollecte, local_icons),
+		layers: {
+			baselayers: {
+				openStreetMap: {
+					name: 'OpenStreetMap',
+					type: 'xyz',
+					url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				}
+			},
+		},
+		events: {
+			map: {
+				enable: ['moveend'],
+				logic: 'emit'
+			}
+		}
 
-                return resultat;
-            };
+	});
 
-            /**
-             * Ajouter d'un cercle pour délimiter la zone d'affichage des marqueurs
-             */
-            var ajouteCercleZoneLimitee = function(lat,lng) {
-
-                leafletData.getMap("mymap").then(
-                    function (map) {
-
-                        //Suppression des marqueurs existants
-                        for(i=0; i<markersCircle.length; i++) {
-                            map.removeLayer(markersCircle[i]);                                                        
-                        }
-                        markersCircle = new Array();
-
-                        var distance = ParamService.getNumberParam('geo.zoneDistance', 1000);
-                        var circle = L.circle([lat, lng], distance, 
-                                {
-                                    color : 'green',
-                                    fillColor : '#c2e47e',
-                                    fillOpacity : 0.15
-                                });
-                        circle.addTo(map);
-                        markersCircle.push(circle);
-
-                        var circle2 = new L.CircleMarker([lat, lng],
-                                {
-                                    rayon : 5,
-                                    color : 'darkgreen',
-                                    fillColor : "green",
-                                    fillOpacity : 0.8
-                                });
-                        circle2.addTo(map);
-                        markersCircle.push(circle2);
-
-                    }
-                );
-               
-            };
-
+	//Récupération des marqueurs Leaflets
+	$scope.labelsModeCollecte = labelsModeCollecte;
 
 });
--- www/js/service/CarteService.js
+++ www/js/service/CarteService.js
@@ -1,168 +1,107 @@
 angular.module('starter.controllers')
 .factory('CarteService', function ($filter, $translate, ParamService) {
-  
-  //GLOBAL DATA
-  // var structuresData = _structuresDatas;
-   
-  var structureCollecteType = [{
-        name : 'Tous',
-        value : '.*'
-    }, {
-        name : "Déchèteries / Ecopoints",
-        value : "modco_decheterie|modco_ecopoint"
-    }, {
-        name : "Encombrants",
-        value : "modco_encombrants_resume"
-    }, {
-        name : "Réemploi",
-        value : "smco_reemp"
-    }, {
-        name : "Vente vrac",
-        value : "ventevrac"
-    }];
 
-  //Retourne Objet L.icon (Leaflet)
-  var iconMapper = function (structure) {
+	/* Retourne tous les conteneurs (au format compatible Angular Leaflet)
+	 * car les conteneurs sont uniquement destinés à être affichés sur la carte Leaflet
+	GLOBAL DATA _containersDatas
 
-      /*var iconTypeMap = {};
-      iconTypeMap[modesCollecte] = '/img/marker_verre.png';
-      iconTypeMap["Entreprise de réemploi"] = '/img/marker_verre.png';
-      iconTypeMap["Conteneur verre, papier"] = '/img/marker_verre_carton.png';    
-      iconTypeMap["Conteneur papier"] = '/img/marker_verre_carton.png';
-      iconTypeMap["Conteneur verre, papier, canettes"] = '/img/marker_verre_carton_plastique.png';
-      iconTypeMap["Conteneur papier, plastique"] = '/img/marker_verre_carton_plastique.png';
-      iconTypeMap["Conteneur verre, plastique"] = '/img/marker_verre_carton_plastique.png';
-      iconTypeMap["Conteneur emballages journaux magazines"] = '/img/marker_verre_carton_plastique.png';*/
-      
-      //LISTE des différentes icônes
-      var iconCustom= {
-                    //iconUrl: '/img/marker_verre.png',
-                    //shadowUrl: '/img/leaf-shadow.png',
-                    iconSize:     [32, 48], // size of the icon
-                    shadowSize:   [32, 48], // size of the shadow
-                    iconAnchor:   [15, 48], // point of the icon which will correspond to marker's location
-                    shadowAnchor: [4, 30],  // the same for the shadow
-                    popupAnchor:  [0, -45], // point from which the popup should open relative to the iconAnchor
-                    iconUrl:structure
-      };
-      // var listModesCollecte = structure.modesCollecte.split(",");
-      /*if (structure.modesCollecte.indexOf("smco_conteneurlerelais,") >= 0) {
-        iconCustom.iconUrl="/img/marker-red.png";
-      } else if ( structure.modesCollecte.indexOf("modco_contembjournmag") >= 0 ||
-                  structure.modesCollecte.indexOf("modco_contverre") >= 0 ||
-                  structure.modesCollecte.indexOf("marker_verre_carton") >= 0) {
-        iconCustom.iconUrl="resources/icons/marker-icon-blue.png";
-        // /img/marker_verre_carton_plastique.png
-      } else {
-        iconCustom.iconUrl="/img/marker-red.png";
-      }*/
+	location = { lat: 47.22, lng:  -1.52}
 
-      return iconCustom;
-      
-  };
+	 */
+	var _getLeafletContainers = function (centerLocation, stCollectMods, localIcons) {
 
-    
-    /* Retourne tous les conteneurs (au format compatible Angular Leaflet)
-     * car les conteneurs sont uniquement destinés à être affichés sur la carte Leaflet
-       GLOBAL DATA _containersDatas    
-      
-      location = { lat: 47.22, lng:  -1.52}
-      
-    */   
-    var _getLeafletContainers = function (centerLocation, stCollectMods) {
+		var listCollectMods = stCollectMods.split(",");
 
-        // stCollectMods = "modco_reemploi";
-        var leafletContainers={};
-        //DISTANCE EN METRES MAX
-        // var maxDistance=2800;
-        
-        var listCollectMods = stCollectMods.split(",");
-        
-        var expFilter =  function(item, index, array) {
+		var expFilter = function (item, index, array) {
 
-            var modeCo = item.modesCollecte;
-            var showTheMarker = false;
-            if (typeof(modeCo)!="undefined") {
-  
-              var listItemCollectMods = modeCo.split(",");
-              for (var i = 0; i<listItemCollectMods.length && showTheMarker===false; i++) {
-                for (var j=0; j<listCollectMods.length && showTheMarker===false; j++) {
-                  if (listCollectMods[j]===listItemCollectMods[i]) {
-                    showTheMarker = true;
-                  }
-                }
-                // showTheMarker = listItemCollectMods[i] in listCollectMods;
-                // showTheMarker = _utilArrayContainObject(listCollectMods, listItemCollectMods[i]);
-              }
-            }
-            return showTheMarker;
-            // return true;
-        };
+			var marqueur = item.carteMarqueur;
+			var showTheMarker = false;
+			if (typeof(marqueur) != "undefined") {
 
-        //Filtre des marqueurs pour le mode de collecte
-        var tmpDatas = _containersDatas.concat(_structuresDatas);
-       // var tmpDatas = _containersDatas;
-        // var tmpDatas = _structuresDatas;
-        var leafletContainersFiltered = $filter('filter')(tmpDatas, expFilter);
-        var latlngInit = L.latLng(centerLocation.lat, centerLocation.lng);
-        var maxDistance = ParamService.getNumberParam("geo.zoneDistance", 1500);
-       
-        for (var i = 0; i < leafletContainersFiltered.length ; i++) {  
-        
-            //SKIP structure invalide
-            var container=leafletContainersFiltered[i];
-            if (container.length === 0 || container.type == '') continue;
+				for (var j = 0; j < listCollectMods.length && showTheMarker === false; j++) {
+					if (listCollectMods[j] === marqueur) {
+						showTheMarker = true;
+					}
+				}
+			}
+			return showTheMarker;
+			// return true;
+		};
 
-            //SKIP structure sans latitude ou longitude
-            if (angular.isUndefined(container.latitude) || angular.isUndefined(container.longitude)) continue;
+		//Filtre des marqueurs pour le mode de collecte
+		var tmpDatas = _containersDatas.concat(_structuresDatas);
+		// var tmpDatas = _containersDatas;
+		// var tmpDatas = _structuresDatas;
+		var markersFiltered = $filter('filter')(tmpDatas, expFilter);
 
-            //SKIP structure trop lointaine
-            var latlngcurrent = L.latLng(parseFloat(container.latitude), parseFloat(container.longitude));
-            var distance = latlngcurrent.distanceTo(latlngInit);
-            if (distance > maxDistance) {
-                continue;
-            }
-        
-            /*var popuptext = "";
-            if (container.type !== null) {
-              popuptext = "<b>" + container.type + "</b><br/>";
-            }
-            if (container.nom !== null) {
-              popuptext = popuptext + container.nom + '<br/>';
-            }*/
-            /*if (container.adresseTemp !== null) {
-              popuptext = popuptext + container.adresseTemp + '<br/>';
-            }*/
-            /*if (container.src !== null) {
-              popuptext = popuptext + "<span style='color:red;font-style: italic;'>Source : " + container.src + '</span><br/>';
-            }*/
-          /*  if (container.src !== null) {
-              popuptext = "Source : " + container.src;
-            }*/
-            //Mapping data MTN => data Angular Leaflet
-            leafletContainer={
-                 lat: parseFloat(container.latitude),  //IMPORTANT : données origine de type string !
-                 lng: parseFloat(container.longitude), //IMPORTANT : données origine de type string !                
-                 message: container.cartePopuptext,
-                 //layer: 'markerLayer',
-                 // icon: iconMapper(container)
-                 icon:iconMapper(container.carteIcon)
-            };
-        
-            leafletContainers[container.code] = leafletContainer;
-        
-        }
+		var leafletContainers = {};
+		for (var i = 0; i < markersFiltered.length; i++) {
+			var leafletContainer = {};
+			var container = markersFiltered[i];
+			//SKIP structure sans latitude ou longitude
+			if (angular.isUndefined(container.latitude) || angular.isUndefined(container.longitude))
+				continue;
 
-        return leafletContainers;
-   
-    };
+			if (container.carteMarqueur == 1) {
+				leafletContainer = {
+					lat: parseFloat(container.latitude),
+					lng: parseFloat(container.longitude),
+					message: container.cartePopuptext,
+					icon: localIcons.reemploi,
+					focus: true,
+					draggable: false
+				};
+			} else if (container.carteMarqueur == 2) {
+				leafletContainer = {
+					lat: parseFloat(container.latitude),
+					lng: parseFloat(container.longitude),
+					message: container.cartePopuptext,
+					icon: localIcons.conteneurs
+				};
+			} else if (container.carteMarqueur == 3) {
+				leafletContainer = {
+					lat: parseFloat(container.latitude),
+					lng: parseFloat(container.longitude),
+					message: container.cartePopuptext,
+					icon: localIcons.decheterie
+				};
+			} else if (container.carteMarqueur == 4) {
+				leafletContainer = {
+					lat: parseFloat(container.latitude),
+					lng: parseFloat(container.longitude),
+					message: container.cartePopuptext,
+					icon: localIcons.composteurs
+				};
+			} else if (container.carteMarqueur == 5) {
+				leafletContainer = {
+					lat: parseFloat(container.latitude),
+					lng: parseFloat(container.longitude),
+					message: container.cartePopuptext,
+					icon: localIcons.conteneurlerelais
+				};
+			} else if (container.carteMarqueur == 6) {
+				leafletContainer = {
+					lat: parseFloat(container.latitude),
+					lng: parseFloat(container.longitude),
+					message: container.cartePopuptext,
+					icon: localIcons.ventevrac
+				};
+			} else if (container.carteMarqueur == 7) {
+				leafletContainer = {
+					lat: parseFloat(container.latitude),
+					lng: parseFloat(container.longitude),
+					message: container.cartePopuptext,
+					icon: localIcons.trisac
+				};
+			}
+			leafletContainers[container.code] = leafletContainer;
 
-    
-    return {
-      
-        getLeafletContainers : _getLeafletContainers,
+		}
+		return leafletContainers;
+	};
 
-    };
-    
-    
+	return {
+		getLeafletContainers: _getLeafletContainers,
+	};
+
 });
