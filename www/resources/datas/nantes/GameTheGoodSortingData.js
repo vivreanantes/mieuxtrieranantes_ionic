33d7924b809cfe863ca7ff541d38e8ae9fd42a1a
Christian Renoulin <crenoulin.prestataire@voyages-sncf.com>
2017-08-25 17:09:18 +0200

    Ajout jeu "Le bon tri"

---
+++ www/resources/datas/nantes/GameTheGoodSortingData.js
@@ -0,0 +1,210 @@
+var _gameTheGoodSortingData = [{
+		"code": "quiz_janvier",
+		"nom": "Quiz spécial janvier (5 questions)",
+		"descr": "Tester vos connaissances sur le tri sur Nantes Métropole.",
+		"indication": "une seule réponse par question.",
+		"bouton": "fiche",
+		"image": "quiz/issiontrix-quiz-1.png",
+		"mots_cles": "quiz",
+		"mots_cles_en": "quiz",
+		"modes_de_collecte": [{
+				"id": "decheterie",
+				"descr": "Déchèterie/écopoint",
+				"image": "quiz/decheterie.png"
+			}, {
+				"id": "bleu",
+				"descr": "Poubelle bleu / Sac bleu",
+				"image": "quiz/decheterie.png"
+			}, {
+				"id": "jaune",
+				"descr": "Poubelle jaune / sac jaune",
+				"image": "quiz/decheterie.png"
+			}, {
+				"id": "composteur"
+				"descr": "Déchèterie/écopoint",
+				"image": "quiz/composteur.png"
+			}, {
+				"id": "retour"
+				"descr": "Retour au point de vente",
+				"image": "quiz/pointdevente.png"
+			}, {
+				"id": "reemploi"
+				"descr": "Réemploi",
+				"image": "quiz/reemploi.png"
+			}
+		],
+		"questions": [{
+				"descr": "Marc de café",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				],
+			}, {
+				"descr": "Serviette en papier",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Appareil photo",
+				"reponses_correctes"[{
+						"id": "pointdevente"
+					}
+				]
+			}, {
+				"descr": "Bouteille huile plastique",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Verre à boire",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Tube dentifrice",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Palette en bois",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Rouge à lèvres",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Boîte à oeufs carton",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Cendre de bois",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Gravat",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Cannette en aluminium",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Peau banane",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Crayon bic",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Laque en sray",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Flacon de shaampoing",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Canapé",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Batterie de voiture",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}, { "id": "pointdevente"}
+				]
+			}, {
+				"descr": "Mégot de cigarette",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Pot de peinture (métal)",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Oreiller",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					},
+					{
+						"id": "conteneurvet"
+					}
+				]
+			}, {
+				"descr": "Epeluchures légumes",
+				"reponses_correctes"[{
+						"id": "composteur"
+					}
+				]
+			}, {
+				"descr": "Feuilles arbres",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Feuilles arbres",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Lampe",
+				"reponses_correctes"[{
+						"id": "decheterie"
+					}
+				]
+			}, {
+				"descr": "Capsule aluminium",
+				"reponses_correctes"[{
+						"id": "pointdevente"
+					}
+				]
+			}, {
+				"descr": "Médicament",
+				"reponses_correctes"[{
+						"id": "pointdevente"
+					}
+				]
+			}, {
+				"descr": "Pot de confiture",
+				"reponses_correctes"[{
+						"id": "conteneurverre"
+					}
+				]
+			}
+		]
+	}
+];
