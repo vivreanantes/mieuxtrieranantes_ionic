4e8808ee344871581fe4857bc18be1b5491d7736
Christian Renoulin <chris-ren@netcourrier.com>
2017-08-27 12:59:39 +0200

    jeu

--- www/js/controller/nantes/TheGoodSortingCtrl.js
+++ www/js/controller/nantes/TheGoodSortingCtrl.js
@@ -11,10 +11,14 @@ angular.module('starter.controllers')
 	$scope.questions = [
 		{name:'nounours',descr:"nounours",image:"nounours.jpg",reponses:["poubelle"]},
 		{name:'arrosoir',descr:"arrosoir",image:"arrosoir.png",reponses:["poubelle","decheterie"]},
+		{name:'pneu',descr:"pneu",image:"pneu.jpg",reponses:["decheterie"]},
 		{name:'peau_banane',descr:"peau de banane",image:"peau_banane.jpg",reponses:["composteur"]}];
 	$scope.reponses = [
 		{id:"decheterie",descr:'Déchèterie',image:"decheterie.jpg"},
 		{id:"composteur",descr:'Composteur',image:"composteur.jpg"},
+		{id:"jaune",descr:'Poubelle jaune / sac jaune, conteneur emaballage, métal, verre, papier-carton',image:"bac_sac_jaune.jpg"},
+		{id:"retour",descr:'Retour au point de vente',image:"composteur.jpg"},
+		{id:"réemploi",descr:'Réemploi, conteneur vêtement, collecteur bouchons',image:"reemploi.jpg"},
 		{id:"poubelle",descr:'Poubelle',image:"bac_sac_bleu.jpg"}];
 	
     $scope.onDragComplete = function(data, evt) {
