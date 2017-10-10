44455c6b91e89af60426dc3e59c34353618b693f
Christian Renoulin <crenoulin.prestataire@voyages-sncf.com>
2017-10-10 16:32:41 +0200

    Conf 2.11

--- www/js/controller/AProposCtrl.js
+++ www/js/controller/AProposCtrl.js
@@ -4,6 +4,9 @@ angular.module('starter.controllers')
 .controller('AProposCtrl', function($scope, $stateParams) {
 
 	//- RM-PA-VERSAPPLI_01 : On modifie la version de l'application en modifiant certains fichiers de configuration.
-	$scope.version = "2.10 - quiz";
+	$scope.version = "2.11 - initiatives locales";
+	$scope.new_in_the_version = "- amélioration paramétrage : on affiche l'adresse configurée et le jour de collecte<br/>amélioration du quiz : rappel des réponses à la fin.<br/>ajout d'une page 'signaler erreur'.<br/>30 nouveaux lieux de vente en vrac sur la carte : magasins demarche réduction déchets par Oasis Environnement, lieux utilisation bouteilles consignées par Bout' à bout'<br/>changements atticles 'Découvrir' : Bout à bout, Solution recyclage, Oasis environnement<br/>changements horaires ou descriptif 'Lieux' : Arbres, Oser Forêt Vivante,<br/>Changements horaires : recyclerie de l'île";
+
+";
 
 });
