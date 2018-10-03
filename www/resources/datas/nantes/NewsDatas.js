e08fb7c2b7b518da292cb5d9577f2bf11496a421
Christian Renoulin <chris-ren@netcourrier.com>
2018-10-03 22:15:04 +0200

    Version 2.14

--- www/resources/datas/nantes/NewsDatas.js
+++ www/resources/datas/nantes/NewsDatas.js
@@ -1,21 +1,21 @@
 var _newsDatas = [{
 		// Miscellaneous Symbols and Pictographs : https://fr.wikipedia.org/wiki/Table_des_caract%C3%A8res_Unicode/U1F300
 		"code": "Actu_01",
-		"nom": "&#x263a; Site internet VracANantes",
-		"nom_en": "Web site VracANantes",
-		"descr": "Sortie du site internet qui recense les lieux de vrac sur Nantes Métropole. Voir <a href=\"javascript:void(0);\" onclick=\"window.open('http://www.vracanantes.fr', '_system', 'location=yes');\">www.vracanantes.fr</a>",
-		"descr_en": "Sortie du site internet qui recense les lieux de vrac sur Nantes Métropole. See <a href=\"javascript:void(0);\" onclick=\"window.open('http://www.vracanantes.fr', '_system', 'location=yes');\">www.vracanantes.fr</a>"
+		"nom": "&#x263a; La Galerie du Zéro Déchet",
+		"nom_en": "La Galerie du Zéro Déchet",
+		"descr": "Un nouveau lieu nantais ouvrira en mars 2019, à proximité des Galerie LaFayette. Il sera ouvert au grand public toute l'année. Voir <a href=\"javascript:void(0);\" onclick=\"window.open('http://www.lagalerieduzerodechet.fr', '_system', 'location=yes');\">www.lagalerieduzerodechet.fr</a>",
+		"descr_en": "A new place in Nantes will open in March 2019, near the Galerie LaFayette. It will be open to the general public all year round.. See <a href=\"javascript:void(0);\" onclick=\"window.open('http://www.lagalerieduzerodechet.fr', '_system', 'location=yes');\">www.lagalerieduzerodechet.fr</a>"
+	}, {
+		"code": "Actu_02",
+		"nom": "&#x1F441; Un site pour la co-réparation en Loire-Atlantique",
+		"nom_en": "&#x1F441; A site for co-repair in Loire-Atlantique",
+		"descr": " Voir <a href=\"javascript:void(0);\" onclick=\"window.open('https://www.coreparation44.fr', '_system', 'location=yes');\" >www.coreparation44.com</a>",
+		"descr_en": "See <a href=\"javascript:void(0);\" onclick=\"window.open('https://www.coreparation44.fr', '_system', 'location=yes');\" >www.coreparation44.com</a>"
 	}, {
 		"code": "Actu_03",
 		"nom": "&#x1f4b2; Le retour de la consigne sur les bouteilles.",
 		"nom_en": "&#x1f4b2; The consigne is back.",
 		"descr": "La nouvelle association Bout'à'Bout fournit des bouteilles consignées aux producteurs. Le magasin O Bôcal propose ces produits. Voir <a href=\"javascript:void(0);\" onclick=\"window.open('http://www.boutabout.org', '_system', 'location=yes');\" >www.boutabout.org</a>",
 		"descr_en": "The new association Bout'à'Bout . See <a href=\"javascript:void(0);\" onclick=\"window.open('http://www.boutabout.org', '_system', 'location=yes');\" >www.boutabout.org</a>"
-	}, {
-		"code": "Actu_04",
-		"nom": "&#x1F441; Débat : les nouvelles bouteilles de lait, non recyclables ?",
-		"nom_en": "&#x1F441; Question : The new bottles of milk, not recyclable ?",
-		"descr": " Voir <a href=\"javascript:void(0);\" onclick=\"window.open('https://www.actu-environnement.com/ae/news/pet-opaque-recyclage-ultimatum-eco-emballages-bouteille-lait-28386.php4', '_system', 'location=yes');\" >www.actu-environnement.com</a>",
-		"descr_en": "See <a href=\"javascript:void(0);\" onclick=\"window.open('https://www.actu-environnement.com/ae/news/pet-opaque-recyclage-ultimatum-eco-emballages-bouteille-lait-28386.php4', '_system', 'location=yes');\" >www.actu-environnement.com</a>"
 	}
 ];
